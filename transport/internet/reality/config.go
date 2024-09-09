61ae2be301785c6f9952a634ccdb593c361b4a39
AliCl0ner <alicloner@hotmail.com>
2024-09-09 17:38:26 +0330

    Update config.go

--- transport/internet/reality/config.go
+++ transport/internet/reality/config.go
@@ -12,6 +12,7 @@ import (
 	"github.com/xtls/xray-core/transport/internet"
 )
 
+// GetREALITYConfig returns a configured *reality.Config based on the current Config instance.
 func (c *Config) GetREALITYConfig() *reality.Config {
 	var dialer net.Dialer
 	config := &reality.Config{
@@ -27,22 +28,27 @@ func (c *Config) GetREALITYConfig() *reality.Config {
 		MaxClientVer: c.MaxClientVer,
 		MaxTimeDiff:  time.Duration(c.MaxTimeDiff) * time.Millisecond,
 
-		NextProtos:             nil, // should be nil
+		NextProtos:             nil, // Nil is expected here
 		SessionTicketsDisabled: true,
 
 		KeyLogWriter: KeyLogWriterFromConfig(c),
 	}
+
 	config.ServerNames = make(map[string]bool)
 	for _, serverName := range c.ServerNames {
 		config.ServerNames[serverName] = true
 	}
+
 	config.ShortIds = make(map[[8]byte]bool)
 	for _, shortId := range c.ShortIds {
 		config.ShortIds[*(*[8]byte)(shortId)] = true
 	}
+
 	return config
 }
 
+// KeyLogWriterFromConfig creates an io.Writer for logging keys if a log file is specified.
+// Returns nil if no log file is specified or if an error occurs while opening the file.
 func KeyLogWriterFromConfig(c *Config) io.Writer {
 	if len(c.MasterKeyLog) <= 0 || c.MasterKeyLog == "none" {
 		return nil
@@ -51,11 +57,14 @@ func KeyLogWriterFromConfig(c *Config) io.Writer {
 	writer, err := os.OpenFile(c.MasterKeyLog, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
 		errors.LogErrorInner(context.Background(), err, "failed to open ", c.MasterKeyLog, " as master key log")
+		return nil // Ensure a nil return on error
 	}
 
 	return writer
 }
 
+// ConfigFromStreamSettings converts internet.MemoryStreamConfig's SecuritySettings to *Config.
+// Returns nil if settings are nil or not of type *Config.
 func ConfigFromStreamSettings(settings *internet.MemoryStreamConfig) *Config {
 	if settings == nil {
 		return nil
